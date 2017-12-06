8169df5b1e5e4eb3103ee9149202525f830366b4
Zachary Jensen <ZJTJensen@yahoo.com>
2017-12-06 10:23:36 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  buttons: Array<boolean>=[]
+  ngOnInit(){
+      for(var i=0;i<11;i++){
+         this.buttons[i]= false;
+     }
+     
+}
+click(i){
+this.buttons[i] = !this.buttons[i];
+}
 }
